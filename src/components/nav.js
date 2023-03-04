9d0f482668b674e0530a0ffa47d19b8154e945f5
Eldhose Iype <eldhoseiypeparavatty@gmail.com>
2023-03-04 14:38:09 +0530

    edit in nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { useState } from 'react';
 import "./nav.css"
 import { Link } from 'react-scroll'
-import About from './about';
-import Projects from './projects';
-import {Skills}  from './skills';
 
 const Nav = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -18,12 +15,12 @@ const Nav = () => {
     return (
         <div className='container1'>
             <h2 className='h2'>Portifolio</h2>
-            <a id="btn" href='#' onClick={toggleMenu}>{menuText}</a>
+            <a id="btn" href='#' onClick={toggleMenu} >{menuText}</a>
             <div className={`right1 ${isOpen ? 'show' : ''}`}>
-              <Link to="skills" id="p1" smooth={true} duration={500} onClick={toggleMenu}>Skills</Link>
-              <Link to="projects" id="p1" smooth={true} duration={500} onClick={toggleMenu}>Projects</Link>
-              <Link to="about" id="p1" smooth={true} duration={500} onClick={toggleMenu}>Contact</Link>
-              <Link to="about" id="p1" smooth={true} duration={500} onClick={toggleMenu}>About</Link>
+              <Link to="skills" id="p1" smooth={true} duration={500} >Skills</Link>
+              <Link to="projects" id="p1" smooth={true} duration={500} >Projects</Link>
+              <Link to="about" id="p1" smooth={true} duration={500} >Contact</Link>
+              <Link to="about" id="p1" smooth={true} duration={500} >About</Link>
 
             </div>
         </div>
